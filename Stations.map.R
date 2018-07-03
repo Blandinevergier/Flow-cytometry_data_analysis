aa30b7eb60bda385c70a0a2bee5b37069e4d3838
Blandinevergier <40200108+Blandinevergier@users.noreply.github.com>
2018-07-03 13:38:25 +0000

    Map of Iceland

--- Stations.map.R
+++ Stations.map.R
@@ -4,7 +4,7 @@ library(ggcyto)
 library(stringr)
 library(car)
 library(rgl)
-
+library(ggplot2)
 library(geo)
 library(devtools)
 library(maps)
@@ -27,12 +27,27 @@ library(ggmap)
 	today <- '20180615'
 	
 	#INFORMATIONS ABOUT THE STATIONS
-	Infos.path <- '/Users/bland/Desktop/Flow-cytometry_data/Summary_stations/hydrostn.csv' #Path of the csv file containing the informations about the stations
+	Infos.path <- '/Users/bland/Desktop/Flow-cytometry_data/Output/Dataframe/20180628_Summary_after_automatic_gating.csv'#Path of the csv file containing the informations about the stations
 	
 	Infos1 <- read.csv(Infos.path, sep=';', header=TRUE)
+	iceland <- read.csv('/Users/bland/Desktop/Flow-cytometry_data/Summary_stations/iceland.csv', sep=';', header=TRUE)
 	
 
 
 Infos1 %>% 
  ggplot(aes(stn.lat, stn.lon, size = stn.max.depth)) +
  geom_point(colour = "red")
+ 
+
+head(iceland)
+glimpse(iceland)
+
+
+
+p <- ggplot(iceland, aes(long, lat)) + labs(x = NULL, y = NULL)
+p1 <- p + geom_point()
+p2 <- p + geom_line()
+p3 <- p + geom_path()
+Infos$Smp.depth <- as.numeric(as.vector(Infos$Smp.depth))
+
+p3 + geom_point(data=Infos, aes(x=as.numeric(as.character(stn.lon)), y=as.numeric(as.character(stn.lat)), size=Abundance)) + facet_wrap(~Smp.depth)
