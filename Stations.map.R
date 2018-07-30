a4ac1b8bdb81995602c6a4f327396c080490ec80
Blandinevergier <40200108+Blandinevergier@users.noreply.github.com>
2018-07-30 13:20:51 +0000

    Update Stations.map.R

--- Stations.map.R
+++ Stations.map.R
@@ -1,5 +1,4 @@
 ###LOAD PACKAGES
-library(flowCore)
 library(ggcyto)
 library(stringr)
 library(car)
@@ -17,37 +16,42 @@ library(ggmap)
 
 
 ###VARIABLES
-
-	Folder.path <- '/Users/bland/Desktop/Flow-cytometry_data/Input/' #Path of the folder containing the FCS Files
-	csv.path <- '/Users/bland/Desktop/Flow-cytometry_data/Output/Dataframe/' #Path of the folder containing the csv file for the results
-	img.path <- '/Users/bland/Desktop/Flow-cytometry_data/Output/Figures/Plots/3D_plots/' #Path of the folder containing the PDF Files for the results
-	csv.name <- "_Abundance_with_all_info_results.csv" #Name of the CSV file containing the results
-	pdf.name <- "_Plots_with_gating.pdf" #Name of the pdf containing the plots with the gates
-	liste.stations <- c('FX5') #List of the keywords of the stations to analyse ###be sure that all the FCS files corresponding to the stations are in the folder and that the keywords correspond to a unique station
-	today <- '20180615'
+	Map.name <- "Spring_2017_Phytoplankton_concentration_around_Iceland.png" #Name of the map
+	img.path <- '/Users/bland/Desktop/Flow-cytometry_data/Output/figures/'#Path of the folder in which the map will be saved
 	
 	#INFORMATIONS ABOUT THE STATIONS
-	Infos.path <- '/Users/bland/Desktop/Flow-cytometry_data/Output/Dataframe/20180628_Summary_after_automatic_gating.csv'#Path of the csv file containing the informations about the stations
+	Infos.path <- '/Users/bland/Desktop/Flow-cytometry_data/Output/Dataframe/20180704_Summary_after_automatic_gating_QC.csv'#Path of the csv file containing the informations about the stations
 	
 	Infos1 <- read.csv(Infos.path, sep=';', header=TRUE)
-	iceland <- read.csv('/Users/bland/Desktop/Flow-cytometry_data/Summary_stations/iceland.csv', sep=';', header=TRUE)
+	iceland <- read.csv('/Users/bland/Desktop/Flow-cytometry_data/Summary_stations/iceland.csv', sep=';', header=TRUE) ###Read the csv file containing the coordinate of Iceland
 	
 
 
-Infos1 %>% 
- ggplot(aes(stn.lat, stn.lon, size = stn.max.depth)) +
- geom_point(colour = "red")
- 
+
 
 head(iceland)
 glimpse(iceland)
 
+Infos <- Infos1[c(which(Infos1$Smp.depth=="0"), which(Infos1$Smp.depth=="10"), which(Infos1$Smp.depth=="20"),which(Infos1$Smp.depth=="30"),which(Infos1$Smp.depth=="50"), which(Infos1$Smp.depth=="100"), which(Infos1$Smp.depth=="200"), which(Infos1$Smp.depth=="400"), which(Infos1$Smp.depth=="600")),]
+Infos$stn.lon <- gsub(",",".",Infos$stn.lon)
+Infos$stn.lat <- gsub(",",".",Infos$stn.lat)
+Infos$Smp.depth <- as.numeric(as.vector(Infos$Smp.depth))
+Infos$Abundance <- as.numeric(as.vector(Infos$Abundance))
+Infos$stn.lat <- as.numeric(as.vector(Infos$stn.lat))
+Infos$stn.lon <- as.numeric(as.vector(Infos$stn.lon))
 
 
-p <- ggplot(iceland, aes(long, lat)) + labs(x = NULL, y = NULL)
+p <- ggplot(iceland, aes(long, lat)) + labs(x = "Longitude [decimal]", y = "Latitude [decimal]") + xlim(-29, -7) + ylim(62.5, 68.5) + theme(panel.background = element_rect(fill = "white", colour = "black"), panel.grid.major = element_line(colour = "grey80"), panel.grid.minor = element_blank())
 p1 <- p + geom_point()
 p2 <- p + geom_line()
-p3 <- p + geom_path()
-Infos$Smp.depth <- as.numeric(as.vector(Infos$Smp.depth))
+p3 <- p + geom_path() + geom_polygon(fill = "grey60", colour="grey60")
 
-p3 + geom_point(data=Infos, aes(x=as.numeric(as.character(stn.lon)), y=as.numeric(as.character(stn.lat)), size=Abundance)) + facet_wrap(~Smp.depth)
+
+
+png.file <- png(paste(img.path,Map.name ,sep =""), width=1200, height=750)	
+png.file
+p3 + geom_point(data=Infos, aes(x=as.numeric(as.character(stn.lon)), y=as.numeric(as.character(stn.lat)), size=Abundance), colour = "yellowgreen", alpha = 0.6) + scale_size_area(max_size = 20) + facet_wrap(~Smp.depth, ncol=2) + labs(title = "Concentration of phytoplankton (events/mL) around Iceland in spring 2017 depending on the depth") + theme(legend.title = element_blank(), legend.key = element_rect(fill = "white", colour = "white"), plot.title = element_text(size = 23, face="bold"), axis.text = element_text(size=15), legend.text = element_text(size=18), axis.title = element_text(size=18), strip.text= element_text(size=18))
+
+
+
+dev.off() #close window
